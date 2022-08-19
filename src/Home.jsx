9a80597d15c11928e03d0523100b349dab15a62d
Waseem <waseemahmed8129@gmail.com>
2022-08-19 10:08:52 +0500

    navbar is added

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Home = () =>{
+  return(
+  <>
+  <h1>Welcome home</h1>
+  </>
+  );
+};
+
+export default Home;
---
+++ src/Services.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Services = () =>{
+  return(
+  <>
+  <h1>Welcome to services</h1>
+  </>
+  );
+};
+
+export default Services;
